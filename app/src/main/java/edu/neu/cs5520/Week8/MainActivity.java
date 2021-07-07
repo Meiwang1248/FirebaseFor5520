836c304f15b0090a1e4cc2b38c4a759667ed6440
Meiwang1248 <meiwang1248@hotmail.com>
2021-07-07 12:45:24 -0700

    Added Cloud Messaging key

--- app/src/main/java/edu/neu/cs5520/Week8/MainActivity.java
+++ app/src/main/java/edu/neu/cs5520/Week8/MainActivity.java
@@ -6,6 +6,8 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String SERVER_KEY = "AAAADSk70sk:APA91bE5pbobY4eklHx46vhsoFJGIMfzqNjgqfywBlcJFPxS-hKwIMgJDnza4DjlAeSkSeCEMoorcH9ie5hEzJo1mlvxuce3bx-EHvmu-WIe7cSCI17Se6YiTEJaPJVfMMKqsF6uWQTW";
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
