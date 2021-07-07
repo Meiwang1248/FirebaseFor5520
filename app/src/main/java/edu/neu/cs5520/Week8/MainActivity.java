39c622338537e7d8094e195f64ca8ace4b89422e
Meiwang1248 <meiwang1248@hotmail.com>
2021-07-06 19:55:20 -0700

    First Commit

---
+++ app/src/main/java/edu/neu/cs5520/Week8/MainActivity.java
@@ -0,0 +1,15 @@
+package edu.neu.cs5520.Week8;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+}
