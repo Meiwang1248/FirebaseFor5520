cd54d06f977ee245bfbc254329a8471698c535e0
Meiwang1248 <meiwang1248@hotmail.com>
2021-07-08 22:02:17 -0700

    Updated Main Activity

---
+++ app/src/main/java/edu/neu/cs5520/Week8/models/User.java
@@ -0,0 +1,27 @@
+package edu.neu.cs5520.Week8.models;
+
+import java.util.HashMap;
+
+public class User {
+
+    public String username;
+    public String token;
+    public int numberOfStickersSent;
+    public HashMap<String, StickerUserPair> stickerUserPairs;
+
+
+    public User() {
+        // Default constructor required for calls to DataSnapshot.getValue(User.class)
+    }
+
+    public User(String username, String token) {
+        this.username = username;
+        this.token = token;
+        this.numberOfStickersSent = 0;
+        this.stickerUserPairs = new HashMap<>();
+
+        // add a welcome smiley
+        this.stickerUserPairs.put("first-welcome", new StickerUserPair("Welcome From Devs!", "smile"));
+    }
+}
+
